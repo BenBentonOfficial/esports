4f8cc0759d5d2f51980885ec83a06e25f2c9ecee
BenBentonOfficial <BenBentonOfficial@gmail.com>
2021-11-12 15:44:16 -0600

    update paddings

--- components/landing.js
+++ components/landing.js
@@ -4,7 +4,7 @@ import styles from "../styles/landing.module.css"
 export default function Landing() {
   return (
     <>
-      <div className="flex flex-col items-center my-80">
+      <div className="flex flex-col text-center lg:my-80 mt-20 mb-40">
         <div className={styles.title}></div>
         <div className="text-6xl font-bold">
           <h1>

24ec50990e635a8daac2759e7a7720ddbc69c265
BenBentonOfficial <BenBentonOfficial@gmail.com>
2021-11-12 15:39:51 -0600

    fix carousel stretching - h-full w-full

--- components/carousel.js
+++ components/carousel.js
@@ -1,15 +1,15 @@
 export default function Carousel() {
   return (
     <>
-      <div class="carousel rounded-box h-80 w-110 lg:mx-40 mx-8 ">
-        <div className="carousel-item">
-          <img className="image-full" src="images/trophy.jpeg"></img>
+      <div class="carousel rounded-box lg:mx-40 mx-8 h-full w-full">
+        <div className="carousel-item h-full w-full">
+          <img className="object-contain" src="images/trophy.jpeg"></img>
         </div>
-        <div className="carousel-item">
-          <img src="images/comp-team.jpeg"></img>
+        <div className="carousel-item h-full w-full">
+          <img className="object-contain" src="images/comp-team.jpeg"></img>
         </div>
-        <div className="carousel-item">
-          <img src="images/comp-team-2.jpeg"></img>
+        <div className="carousel-item h-full w-full">
+          <img className="object-contain" src="images/comp-team-2.jpeg"></img>
         </div>
       </div>
     </>

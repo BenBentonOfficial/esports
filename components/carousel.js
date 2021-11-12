ae8ebb12946c41505140888d420ad5d4105f0f3a
BenBentonOfficial <BenBentonOfficial@gmail.com>
2021-11-12 15:33:18 -0600

    try

--- components/carousel.js
+++ components/carousel.js
@@ -3,7 +3,7 @@ export default function Carousel() {
     <>
       <div class="carousel rounded-box h-80 w-110 lg:mx-40 mx-8 ">
         <div className="carousel-item">
-          <img className="" src="images/trophy.jpeg"></img>
+          <img className="image-full" src="images/trophy.jpeg"></img>
         </div>
         <div className="carousel-item">
           <img src="images/comp-team.jpeg"></img>

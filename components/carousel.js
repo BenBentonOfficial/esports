21ce7004018198db5f47ffddb9152ec52050a5e3
BenBentonOfficial <BenBentonOfficial@gmail.com>
2021-11-15 08:39:05 -0600

    really fix carousel

--- components/carousel.js
+++ components/carousel.js
@@ -2,13 +2,13 @@ export default function Carousel() {
   return (
     <>
       <div class="carousel rounded-box lg:mx-40 mx-8">
-        <div className="carousel-item  sm:w-full lg:w-4/5 xl:w-2/5">
+        <div className="carousel-item h-auto w-full lg:w-4/5 xl:w-2/5">
           <img className="object-contain" src="images/trophy.jpeg"></img>
         </div>
-        <div className="carousel-item sm:w-full lg:w-4/5 xl:w-2/5">
+        <div className="carousel-item h-auto w-full lg:w-4/5 xl:w-2/5">
           <img className="object-contain" src="images/comp-team.jpeg"></img>
         </div>
-        <div className="carousel-item sm:w-full lg:w-4/5 xl:w-2/5">
+        <div className="carousel-item h-auto w-full lg:w-4/5 xl:w-2/5">
           <img className="object-contain" src="images/comp-team-2.jpeg"></img>
         </div>
       </div>
